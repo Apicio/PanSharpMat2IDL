597b387a565371f7667822f2de791caeaedf413f
Gianluigi Mucciolo <th3g91@gmail.com>
2016-04-20 14:05:12 +0200

    Aggiunto file matlab per la lettura del geotiff, estratte bande di interesse, bisogna convertire tutto in hdf

---
+++ DatasetExtraction.m
@@ -0,0 +1,16 @@
+clc; clear all; close all;
+path1 = '../Dataset/EO1A1750842002090110PY_B01_L1T.TIF';
+pan = GEOTIFF_READ(path1);
+path2 = '../Dataset/EO1A1750842002090110PY_B02_L1T.TIF';
+ms1p = GEOTIFF_READ(path2);
+path3 = '../Dataset/EO1A1750842002090110PY_B03_L1T.TIF';
+ms1 = GEOTIFF_READ(path3);
+path4 = '../Dataset/EO1A1750842002090110PY_B04_L1T.TIF';
+ms2 = GEOTIFF_READ(path4);
+path5 = '../Dataset/EO1A1750842002090110PY_B05_L1T.TIF';
+ms3 = GEOTIFF_READ(path5);
+path6 = '../Dataset/EO1A1750842002090110PY_B06_L1T.TIF';
+ms4 = GEOTIFF_READ(path6);
+
+imshow(flipud(pan.z), 'xdata', pan.x, 'ydata', pan.y);
+
